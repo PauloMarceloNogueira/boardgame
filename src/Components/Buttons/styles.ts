a0d779652156137af3362667e2540ebce396e35c
Paulo Nogueira <paulonogueira@aplic.com.br>
2021-04-01 00:22:40 -0300

    Updates

--- src/Components/Buttons/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Button = styled.button`
-  padding: 8px;
-  border-radius: 4px;
-  min-height: 32px;
-  padding: 12px 8px;
-  margin-right: 8px;
-`;
--- src/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ButtonsContainer = styled.div`
-  padding-top: 32px;
-  padding-left: 24px;
-  position: fixed;
-`;
